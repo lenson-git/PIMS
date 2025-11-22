be7af0d596b4eac5866a0f50e32f55170bd4d520
lenson-git <lenson.sz@gmail.com>
2025-11-22 16:57:32 +0700

    回退

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 export const SUPABASE_URL = 'https://mgcfsinockiucyvptluv.supabase.co'
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nY2ZzaW5vY2tpdWN5dnB0bHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDk0NDMsImV4cCI6MjA3OTAyNTQ0M30.gWn0UWWmsc2xhK2zo4tx0yK1eATbtP2-dUC8PQZ2Hm4'
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
